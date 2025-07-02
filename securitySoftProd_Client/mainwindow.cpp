1bc13493f4265184440f6623e2918cfe76ad0e2d
Hyun-nine-CH <ttmstym234@gmail.com>
2025-07-02 23:19:06 +0900

    lllllll

--- securitySoftProd_Client/mainwindow.cpp
+++ securitySoftProd_Client/mainwindow.cpp
@@ -1,11 +1,16 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include "clientinfoform.h"
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    ClientInfoForm* clientInfo = new ClientInfoForm(this);
+    ui->tabWidget->addTab(clientInfo, tr("주문 정보"));
 }
 
 MainWindow::~MainWindow()
